6af72f1f3d2fb6dbd21bbb3311931dfa0c34b4b9
Ryan Odegard <rsodegard@gmail.com>
2019-09-25 22:33:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-   random_hash = { :railroads =>  { :rent_in_dollars => { :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200   }, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {},
+   random_hash = { :railroads =>  { :rent_in_dollars => { :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200   }, :names => {:reading_railroad => {"mortgage_value": "$100"}, :pennsylvania_railroad => {}, :b_and_o_railroad => {},
       :shortline_railroad => {} }}}
   p random_hash
 end
