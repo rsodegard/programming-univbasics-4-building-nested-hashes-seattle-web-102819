91cf8da03677ba20f3dad54fc350568125b10173
Ryan Odegard <rsodegard@gmail.com>
2019-09-25 22:09:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  random_hash = { :railroads =>  { :rent_in_dollars => 4 } }
+  random_hash = { :railroads =>  { :rent_in_dollars => { :key1 => "one",   } } }
   p random_hash
 end
 
