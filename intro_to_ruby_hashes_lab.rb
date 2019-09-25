331693471012166cc2e98b03a76a8bf3237134cf
Ryan Odegard <rsodegard@gmail.com>
2019-09-25 22:08:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,19 +4,13 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
   random_hash = { :railroads =>  { :pieces => 4 } }
   p random_hash
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  random_hash = { :rent_in_dollars =>  { :pieces => 4 } }
+  p random_hash
 end
 
 def monopoly_with_fourth_tier
