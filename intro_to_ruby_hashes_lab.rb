c13916ec8c9b0f036c8bac4ff87e84677244bc4e
Ryan Odegard <rsodegard@gmail.com>
2019-09-25 22:06:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  random_hash = { :railroads =>  { :pieces => } }
+  random_hash = { :railroads =>  { :pieces => "x" } }
   p random_hash
 end
 
